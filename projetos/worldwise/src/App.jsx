c5bc1a5b4eb273d1f7689efbda777704be5a0fe8
Thiago Santos <thiago.santos@logicasolucoes.com.br>
2024-03-20 09:32:39 -0300

    fim da seção 17

--- projetos/worldwise/src/App.jsx
+++ projetos/worldwise/src/App.jsx
@@ -1,12 +1,38 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import PageNotFound from './pages/PageNotFound';
 import Homepage from './pages/Homepage';
 import AppLayout from './pages/AppLayout';
 import Login from './pages/Login';
+import CityList from './components/CityList';
+import { useEffect, useState } from 'react';
+import CountryList from './components/CountryList';
+import City from './components/City';
+import Form from './components/Form';
+
+const BASE_URL = 'http://localhost:9000';
 
 function App() {
+	const [cities, setCities] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	useEffect(() => {
+		async function fetchCities() {
+			try {
+				setIsLoading(true);
+				const res = await fetch(BASE_URL + '/cities');
+				const data = await res.json();
+				setCities(data);
+			} catch {
+				alert('There was an error');
+			} finally {
+				setIsLoading(false);
+			}
+		}
+		fetchCities();
+	}, []);
+
 	return (
 		<BrowserRouter>
 			<Routes>
@@ -15,16 +41,24 @@ function App() {
 				<Route path="product" element={<Product />} />
 				<Route path="login" element={<Login />} />
 				<Route path="app" element={<AppLayout />}>
-					<Route index element={<p> List of cities</p>} />
-					<Route path="cities" element={<p> List of cities</p>} />
+					<Route index element={<Navigate replace to="cities" />} />
+					<Route
+						path="cities"
+						element={<CityList cities={cities} />}
+					/>
+					<Route path="cities/:id" element={<City />} />
 					<Route
 						path="countries"
-						element={<p> List of countries</p>}
+						element={
+							<CountryList
+								cities={cities}
+								isLoading={isLoading}
+							/>
+						}
 					/>
-					<Route path="form" element={<p>Form</p>} />
+					<Route path="form" element={<Form />} />
 				</Route>
 
-				<Route path="form" element={<p> Form</p>} />
 				<Route path="*" element={<PageNotFound />} />
 			</Routes>
 		</BrowserRouter>
--- projetos/worldwise/src/components/Map.jsx
+++ projetos/worldwise/src/components/Map.jsx
@@ -1,7 +1,31 @@
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
-	return <div className={styles.mapContainer}></div>;
+	const navigate = useNavigate();
+	const [searchParams, setSearchParams] = useSearchParams();
+	const lat = searchParams.get('lat');
+	const lng = searchParams.get('lng');
+
+	return (
+		<div
+			className={styles.mapContainer}
+			onClick={() => {
+				navigate('form');
+			}}
+		>
+			<h1>
+				Position: {lat}, {lng}
+			</h1>
+			<button
+				onClick={() => {
+					setSearchParams({ name: 'fulano' });
+				}}
+			>
+				Change
+			</button>
+		</div>
+	);
 }
 
 export default Map;
